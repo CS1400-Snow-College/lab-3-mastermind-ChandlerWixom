60c84d402d28b81d933ae9ef685825f7b54e6910
Chandler Wixom <chandlerj.wixom@gmail.com>
2025-02-12 13:19:31 -0700

    Greating

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// Chandler Wixom, 2/12/2025, Lab 3 Mastermind, CS 1405
-Console.WriteLine("Hello, World!");
+Console.WriteLine("In this game a string of 4 letters containing only a - g has been created\nNo letter is repeated twice and the order is completly random\nYou are to guess the order and placement of these letters");
--- obj/Debug/net9.0/lab-3-mastermind-ChandlerWixom.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-3-mastermind-ChandlerWixom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-3-mastermind-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d9c18d4932918fb1138a246d732f888a9d06c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f9381fe2783cd21a1962b303f090cb84b4c814")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-3-mastermind-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-3-mastermind-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
