24428abba8609c02cc07d86e9357f7e455a8ebf4
Chandler Wixom <chandlerj.wixom@gmail.com>
2025-02-12 15:48:10 -0700

    Basic Guessing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
 ﻿// Chandler Wixom, 2/12/2025, Lab 3 Mastermind, CS 1405
 Console.WriteLine("In this game a string of 4 letters containing only a - g has been created\nNo letter is repeated twice and the order is completly random\nYou are to guess the order and placement of these letters");
+
+
+int? randomInt = null;
+Random tempRandom = new Random();
+string egg = ""; // creates the string that we add to later
+while (egg.Length <4) // stops picking leters after it reaches 4 letters
+{
+int tempIntRandom = tempRandom.Next(97,103); //picks random number in the range of the leters we are usings asci numbers a - g, 97 - 103
+char randomChar = (char) tempIntRandom; // changes the randum number to a character 
+
+if (!(egg.Contains(randomChar))) // checks if the string contains the character rolled and if it isnt already in it it will be added
+{
+egg = egg + randomChar; // you can add a character to a string !!! like math but letters
+}
+}
+
+Console.ReadKey(true);
+
+
+Console.WriteLine(egg);
+string playerGuess;
+do
+{
+playerGuess = Console.ReadLine();
+if (playerGuess != egg)
+    Console.WriteLine("Sorry thats wrong");
+}
+while (playerGuess != egg);
--- obj/Debug/net9.0/lab-3-mastermind-ChandlerWixom.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-3-mastermind-ChandlerWixom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-3-mastermind-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f9381fe2783cd21a1962b303f090cb84b4c814")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60c84d402d28b81d933ae9ef685825f7b54e6910")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-3-mastermind-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-3-mastermind-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
